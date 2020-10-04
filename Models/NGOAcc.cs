e3da991f08d0f41c5d6fda4c5006bc169db744a5
Noctora <xenorite@gmail.com>
2020-10-04 15:22:23 +0800

    Add project files.

---
+++ Models/NGOAcc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VibrantVoluntaire2.Models
+{
+    public class NGOAcc
+    {
+        public int NGOId { get; set; }
+        public string NGOname { get; set; }
+        public string vision { get; set; }
+        public string mission { get; set; }
+        public string objective { get; set; }
+        public string HQlocation { get; set; }
+        public string status { get; set; }
+        public string websiteaddress { get; set; }
+        public string emailaddress { get; set; }
+        public string establisheddate { get; set; }
+        public string SMlink { get; set; }
+    }
+
+
+}
